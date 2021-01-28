9da317337238a7b42c36cfeb5a52d01ac9980b0f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-01-28 14:12:52 -0500

    Update NamedTupleTools.jl

--- src/NamedTupleTools.jl
+++ src/NamedTupleTools.jl
@@ -10,7 +10,8 @@ see [`namedtuple`](@ref),
     [`select`](@ref), [`delete`](@ref), [`separate`](@ref)
     [`merge_recursive`](@ref),
     [`field_count`](@ref), [`field_names`](@ref), [`field_types`](@ref),
-    [`field_values`](@ref)
+    [`field_values`](@ref),
+    [`newstruct`](@ref), [`construct`](@ref)
 """ NamedTupleTools
 
 module NamedTupleTools
@@ -20,7 +21,8 @@ export namedtuple, @namedtuple,
     issame, ≅, canonical, 
     select, delete, separate,
     merge_recursive,
-    field_count, field_names, field_types, field_values
+    field_count, field_names, field_types, field_values,
+    newstruct, construct
 
 using OrderedCollections
 
@@ -124,6 +126,21 @@ namedtuple(names::NTuple{N,String}, values::NTuple{N,Any}) where N =
 function namedtuple(v::Vector{<:Pair{String}})
     N = length(v)
     NamedTuple{ntuple(i -> Symbol(v[i][1]), N)}(ntuple(i -> v[i][2], N))
+end
+
+
+# from Sebastian Pfitzner (on Slack)
+macro namedtuple(vars...)
+   args = Any[]
+   for v in vars
+       if Meta.isexpr(v, :(=)) || Meta.isexpr(v, :...)
+           push!(args, esc(v))
+       else
+           push!(args, Expr(:(=), esc(v), esc(v)))
+       end
+   end
+   expr = Expr(:tuple, Expr(:parameters, args...))
+   return expr
 end
 
 """
@@ -278,15 +295,26 @@ Base.convert(::Type{LittleDict}, x::NamedTuple) =
 Base.convert(::Type{T}, x::NamedTuple) where {T<:AbstractDict} =
     T(convert(Vector{Pair}, x))
 
-# conversion to a struct type and conversion to a struct instance
+# NamedTuples -> structs
+"""
+    construct(<struct type>, NamedTuple)
 
+Construct an instance of the struct using the values from the NamedTuple.
+- unchecked precondition: field types mutually conform
+"""
+construct(T::DataType, x::NamedTuple) = T(field_values(x)...)
+
+"""
+    newstruct(Symbol|String, Type{NamedTuple}|NamedTuple)
+
+Construct an instance of the struct using the values from the NamedTuple.
+- unchecked precondition: field types mutually conform
+"""
 function newstruct(sname::Symbol, x::Type{NamedTuple{N,T}}) where {N,T}
      newstruct(sname, field_names(x), field_types(x))
 end
-
 newstruct(sname::Symbol, x::NamedTuple) = newstruct(sname, typeof(x))
-
-structify(T::DataType, x::NamedTuple) = T(field_values(x)...)
+newstruct(sname::String, x) = newstruct(Symbol(sname), x)
 
 newstruct(sname::Symbol, names::NTuple{N,Symbol}, types::NTuple{N,Type}) where N = 
     eval(eval(parsedstruct(sname, names, types)))
@@ -305,410 +333,6 @@ parseable_struct(structname, names, types) =
              map((x,y) -> Expr(:(::), x, y), $names, $types)...
         )
    )"
-
-	
-end  # NamedTupleTools
-
-
-old
-fieldvalues,
-    @namedtuple,
-       issame,  ≅,
-       namedtuple, isprototype, prototype,
-       fieldvalues, fieldvalues_fast,
-       merge_recursive,
-       separate,
-       delete,
-       select,
-       ntfromstruct, structfromnt,
-       @structfromnt
-
-#=
-import Base: fieldnames, values, merge, split
-
-if isdefined(Base, :fieldtypes)
-     import Base: fieldtypes
-end
-=#
-
-"""
-     issame(nt1, nt2)
-     nt ≊ nt2
-
-field order independent equality
-
-- issame((a=1, b=2), (b=2, a=1))
-- (a=1, b=2) ≅ (b=2, a=1)
-""" issame, ≅
-
-function issame(x::NamedTuple{N,T}, y::NamedTuple{N1,T1}) where {N,T,N1,T1}
-    length(N) === length(N1) &&
-    foldl(&, foldl(.|, ((n .== N) for n=N1))) &&
-    foldl(&, (getfield(x,k) === getfield(y,k) for k=N))
-end
-
-const ≅ = issame
-
-function Base.sort(x::NamedTuple{N,T}) where {N,T}
-    names = Tuple(sort([N...]))
-    return NamedTuple{names}(x)
-end
-
-# internal support for low level manipulation
-
-# accept comma delimited values
-NamedTuple{T}(xs...) where {T} = NamedTuple{T}(xs)
-
-# obtain aspects of a NamedTuple
-
-"""
-   fieldnames( namedtuple )
-   fieldnames( typeof(namedtuple) )
-
-Retrieve, as symbols, the name of each field in appearance (first..last) order.
-- Note: for any nested field, this obtains only the name of top-level field.
-
-Technical note: With some applications, this function is used heavily.
-Fortunately, the operation is completely determined by the argument's type.
-"""
-Base.fieldnames(x::NamedTuple{N,T}) where {N,T} = fieldnames(NamedTuple{N,T})
-
-"""
-    field_types( namedtuple )
-    field_types( typeof(namedtuple) )
-
-Retrieve the values' types as `Tuple{<types>}`.
-
-see: [`fieldtypes`](@ref)
-"""
-field_types(x::T) where {N, S, T<:NamedTuple{N,S}} = S
-field_types(::Type{T}) where {N, S<:Tuple, T<:NamedTuple{N,S}} = S
-field_types(::Type{T}) where {N, T<:NamedTuple{N}} = NTuple{length(N),Any}
-			
-"""
-    fieldtypes( namedtuple )
-    fieldtypes( typeof(namedtuple) )
-
-Retrieve the values' types as a tuple of types `(<types>,)`.
-
-see: [`field_types`](@ref)
-"""			
-fieldtypes(x::T) where {N, S, T<:NamedTuple{N,S}} = detuple(S)
-fieldtypes(::Type{T}) where {N, S<:Tuple, T<:NamedTuple{N,S}} = detuple(S)
-fieldtypes(::Type{T}) where {N, T<:NamedTuple{N}} =
-    ntuple(i->Any, length(N))
-
-fieldtypes(x::T) where {T} = fieldtypes(T) # for structs
-			
-"""
-    fieldvalues(x)
-
-obtain values assigned to fields of a struct type (in field order)
-- `NamedTuples` and `struct`s are struct types
-"""
-function fieldvalues(x::T) where {T}
-    getfield.((x,), fieldnames(T))
-end
-
-"""
-    fieldvalues_fast
-	
-obtain values assigned to fields (in field order)
-
-- This generates new type-specific functions
-- Use spairingly, for time-critical situations
-""" fieldvalues_fast
-
-@generated fieldvalues_fast(x) = Expr(:tuple, (:(x.$f) for f=fieldnames(x))...)
-	
-#=	
-function fieldvalues(x::T) where {T}
-     isstructtype(T) && return unsafe_fieldvalues(x)
-     throw(ArgumentError("$(T) is not a struct type"))
-end
-
-unsafe_fieldvalues(x::T) where {T} = getfield.(Ref(x), fieldnames(T))
-=#
-	
-namedtuple(x::DataType) = ntfromstruct(x)
-
-function ntfromstruct(x::T) where {T}
-    !isstructtype(T) && throw(ArgumentError("$(T) is not a struct type"))
-    names = fieldnames(T)
-    values = getfield.((x,), names)
-    return NamedTuple{names}(values)
-end
-
-
-
-"""
-    retuple( (_) )
-
-Generate a `Tuple` with the given internal types as a `Tuple{_}`.
-"""
-retuple(x::Tuple) = Tuple{x...,}
-
-
-"""
-    detuple( Tuple{_} )
-
-Retrieve the types that are internal to the `Tuple` as a (_).
-"""
-
-
-structfrom(structname::Symbol, x::Type{NamedTuple{N,T}) where {N,T} =
-     structfrom(structname, field_names(nt), field_types(nt))
-
-structfrom(structname::Symbol, names::NTuple{N,Symbol}, types::NTuple{N,Type}) = 
-    eval(eval(Meta.parse(struct_from(structname, names, types))))
-
-# Expr part from Fredrik Ekre
-struct_from(structname, names, types) =
-    "Expr(:struct,
-        false,
-        Expr(:curly,
-             :$structname
-        ),
-        Expr(:block,
-             map((x,y) -> Expr(:(::), x, y), $names, $types)...
-        )
-   )"
-
-
-detuple(::Type{T}) where {T<:Tuple} = Tuple(T.parameters)
-	
-# an instance of type S, a Struct
-function structfromnt(::Type{S}, x::NT) where {S, N, T, NT<:NamedTuple{N,T}}
-     names = N
-     values = fieldvalues(x)
-     if fieldnames(S) != names
-          throw(ErrorException("fields in ($S) do not match ($x)"))
-     end
-     return S(values...,)
-end
-
-# the Struct itself
-function struct_from(structname::Union{Symbol, String}, nt::NamedTuple{N,T}) where {N,T}
-    sname = Symbol(structname)
-    names = N
-    types = detuple(T)
-    tostruct = Meta.parse(struct_from(sname, names, types))
-    result = eval(tostruct) # generate Struct
-    return result
-end
-
-macro structfromnt(sname, nt)
-    :( eval(structfromnt($(esc(sname)), $(esc(nt)))) )
-end
-
-function structfrom(
-
-structfrom(structname, names, types) = eval(eval(Meta.parse(struct_from(structname, names, types))))
-
-"""
-    namedtuple(namesforvalues, valuesfornames)
-"""
-
-# from kristoffer.carlsson
-
-@inline function namedtuple(namesforvalues::NTuple{N,Symbol}, valuesfornames) where {N}
-    N == length(valuesfornames) || throw(ErrorException("lengths must match"))
-    return (; zip(namesforvalues, valuesfornames)...,)
-end
-
-@inline function namedtuple(namesforvalues::Vector{Symbol}, valuesfornames)
-    length(namesforvalues) == length(valuesfornames) || throw(ErrorException("lengths must match"))
-     return (; zip(namesforvalues, valuesfornames)...,)
-end
-
-namedtuple(namesforvalues::Vector{S}, valuesfornames) where {N,S<:AbstractString} =
-    namedtuple(Symbol.(namesforvalues), valuesfornames)
-
-namedtuple(namesforvalues::NTuple{N,S}, valuesfornames) where {N,S<:AbstractString} =
-    namedtuple(Symbol.(namesforvalues), valuesfornames)
-
-
-"""
-    namedtuple(  name1, name2, ..  )
-    namedtuple( (name1, name2, ..) )
-    namedtuple(  namedtuple )
-
-Generate a NamedTuple prototype by specifying or obtaining the fieldnames.
-The prototype is applied to fieldvalues, giving a completed NamedTuple.
-
-# Example
-
-julia> ntprototype = namedtuple( :a, :b, :c )
-
-NamedTuple{(:a, :b, :c),T} where T<:Tuple
-
-julia> nt123 = ntprototype(1, 2, 3)
-
-(a = 1, b = 2, c = 3)
-
-julia> ntAb3 = ntprototype("A", "b", 3)
-
-(a = "A", b = "b", c = 3)
-
-see: [`isprototype`](@ref)
-"""
-namedtuple(names::NTuple{N,Symbol}) where {N} = NamedTuple{names}
-namedtuple(names::Vararg{Symbol}) = NamedTuple{names}
-namedtuple(names::NTuple{N,String}) where {N}  = namedtuple(Symbol.(names))
-namedtuple(names::Vararg{String}) = namedtuple(Symbol.(names))
-namedtuple(names::T) where {T<:AbstractVector{Symbol}} = namedtuple(names...,)
-namedtuple(names::T) where {T<:AbstractVector{String}} = namedtuple(Symbol.(names))
-
-namedtuple(nt::T) where {N,V,T<:NamedTuple{N,V}} = NamedTuple{N}
-
-"""
-    prototype(namedtuple)
-    prototype(typeof(namedtuple))
-
-provides the prototype `NamedTuple{names, T} where T<:Tuple`
-    - `names` is a tuple of symbols
-"""
-prototype(::NamedTuple{A,B}) where {A,B} = NamedTuple{A}
-prototype(::Type{NamedTuple{A,B}}) where {A,B} = NamedTuple{A}
-
-"""
-    isprototype( ntprototype )
-    isprototype( namedtuple  )
-
-Predicate that identifies NamedTuple prototypes.
-
-see: [`namedtuple`](@ref)
-"""
-isprototype(::Type{T}) where {T<:NamedTuple} = eltype(T) === Any
-isprototype(nt::T) where {T<:NamedTuple} = false
-isprototype(::Type{UnionAll}) = false
-
-"""
-   delete(namedtuple, symbol(s)|Tuple)
-   delete(ntprototype, symbol(s)|Tuple)
-
-Generate a namedtuple [ntprototype] from the first arg omitting fields present in the second arg.
-
-see: [`merge`](@ref)
-"""
-delete(a::NamedTuple, b::Symbol) = Base.structdiff(a, namedtuple(b))
-delete(a::NamedTuple, b::NTuple{N,Symbol}) where {N} = Base.structdiff(a, namedtuple(b))
-delete(a::NamedTuple, bs::Vararg{Symbol}) = Base.structdiff(a, namedtuple(bs))
-
-delete(::Type{T}, b::Symbol) where {S,T<:NamedTuple{S}} = namedtuple((Base.setdiff(S,(b,))...,))
-delete(::Type{T}, b::NTuple{N,Symbol}) where {S,N,T<:NamedTuple{S}} = namedtuple((Base.setdiff(S,b)...,))
-delete(::Type{T}, bs::Vararg{Symbol}) where {S,N,T<:NamedTuple{S}} = namedtuple((Base.setdiff(S,bs)...,))
-
-"""
-    separate(namedtuple, symbol(s)|Tuple)
-
-Generate two namedtuples, the first with only the fields in the second arg, the
-second with all but the fields in the second arg, such that
-`merge(split(nt, ks)...) == nt` when `ks` contains the first fields of `nt`.
-"""
-separate(nt::NamedTuple, ks::Symbol) = separate(nt, (ks,))
-separate(nt::NamedTuple, ks) = select(nt, ks), delete(nt, ks)
-
-Base.split(nt::NamedTuple, ks) = error("use `separate` instead of `split`")
-
-"""
-   select(namedtuple, symbol(s)|Tuple)
-   select(ntprototype, symbol(s)|Tuple)
-
-Generate a namedtuple [ntprototype] from the first arg, including only fields present in the second arg.
-
-see: [`merge`](@ref)
-"""
-
-function select(nt::NamedTuple, keepkeys::Tuple{Vararg{Symbol}})
-    syms = filter(x->x!==:_,map(k->in(k,keepkeys) ? k : :_ ,keys(nt)))
-    return NamedTuple{syms}(nt)
-end
-
-function remove(nt::NamedTuple, omitkeys::Tuple{Vararg{Symbol}})
-    syms = filter(x->x!==:_,map(k->in(k,omitkeys) ? (:_) : k ,keys(nt)))
-    return NamedTuple{syms}(nt)
-end
-
-# from a discourse post by Jeff Bezanson
-select(nt::NamedTuple, keys::Tuple{Vararg{Symbol}}) = NamedTuple{keys}(nt)
-# developed therefrom
-select(nt::NamedTuple, keys::Vararg{Symbol}) = NamedTuple{keys}(nt)
-select(nt::NamedTuple, keys::AbstractVector{Symbol}) = NamedTuple{Tuple(keys)}(nt)
-select(nt::NamedTuple, nt4keys::NamedTuple) = select(nt, keys(nt4keys))
-#=
-
-julia> Base.fieldnames(::Type{NamedTuple{N,T}}) where {N,T} = N
-
-julia> Base.fieldnames(x::NamedTuple{N,T}) where {N,T} = N
-
-julia> """
-           flatten(args...) -> ::Tuple
-       Flatten one or more tuples into a single tuple, such that every element of that tuple is itself not a tuple, otherwise it would also be expanded (i.e. flattened).
-       """
-"    flatten(args...) -> ::Tuple\nFlatten one or more tuples into a single tuple, such that every element of that tuple is itself not a tuple, otherwise it would also be expanded (i.e. flattened).\n"
-
-julia> flatten(x::Any) = (x,)
-flatten (generic function with 4 methods)
-
-julia> flatten(t::Tuple{}) = ()
-flatten (generic function with 4 methods)
-
-julia> flatten(t::Tuple) = (flatten(t[1])..., flatten(Base.tail(t))...)
-flatten (generic function with 4 methods)
-
-julia> flatten(x, r...) = (flatten(x)..., flatten(r)...)
-flatten (generic function with 4 methods)
-
-julia> nms=(unique(flatten(fieldnames.((tnt1,tnt2))))...,)
-(:a, :c, :b, :d, :e)
-
-julia> nms=(unique(flatten(fieldnames.((nt1,nt3))))...,)
-(:a, :c, :d)
-=#
-
-#=
-   flatten is from https://github.com/Jutho/TupleTools.jl
-
-   "Flatten one or more tuples into a single tuple,
-    such that every element of that tuple is itself not a tuple,
-    otherwise it would also be expanded (i.e. flattened)."
-=#
-flatten(x::Any) = (x,)
-flatten(t::Tuple{}) = ()
-flatten(t::Tuple) = (flatten(t[1])..., flatten(Base.tail(t))...)
-flatten(x, r...) = (flatten(x)..., flatten(r)...)
-
-#=
-uniquenames(xs)
- obtains the fieldnames present in all xs..., as a unique flattened tuple
-=#
-uniquenames(nts::Tuple{Vararg{T}}) where {T} =
-  (unique(flatten(fieldnames.(nts)))...,)
-
-tuple_unique(xs...) = (unique(xs)...,)
-
-#=
-
-@pure function merge_names(an::Tuple{Vararg{Symbol}}, bn::Tuple{Vararg{Symbol}})
-    @nospecialize an bn
-    names = Symbol[an...]
-    for n in bn
-        if !sym_in(n, an)
-            push!(names, n)
-        end
-    end
-    (names...,)
-end
-
-@pure function merge_types(names::Tuple{Vararg{Symbol}}, a::Type{<:NamedTuple}, b::Type{<:NamedTuple})
-    @nospecialize names a b
-    bn = _nt_names(b)
-    return Tuple{Any[ fieldtype(sym_in(names[n], bn) ? b : a, names[n]) for n in 1:length(names) ]...}
-end
-
-=#
-
 
 
 """
@@ -801,99 +425,5 @@ end
 
 merge_recursive(nt1::NamedTuple, nt2::NamedTuple, nts...) =
     merge_recursive(merge_recursive(nt1, nt2), nts...)
-
-#=
-function firstin(x::NamedTuple{N,T}) where {N,T}
-    name = first(N)
-    value = first(x)
-    return NamedTuple{(name,)}((value,),)
-end
-
-function firstrest(x::NamedTuple{N,T}) where {N,T}
-    isempty(x) && return x
-    name = first(N)
-    value = first(x)
-    firstnt = NamedTuple{(name,)}((value,),)
-    names = Base.diff_names(fieldnames(x),(name,))
-    isempty(names) && return firstnt
-    values = getfield.(Ref(x), names)
-    restnt = NamedTuple{names}(values)
-    return firstnt, restnt
-end
-=#
-
-"""
-    split(namedtuple, symbol(s)|Tuple)
-
-Generate two namedtuples, the first with only the fields in the second arg, the
-second with all but the fields in the second arg, such that
-`merge(split(nt, ks)...) == nt` when `ks` contains the first fields of `nt`.
-"""
-split(nt::NamedTuple, ks::Symbol) = split(nt, (ks,))
-split(nt::NamedTuple, ks) = select(nt, ks), delete(nt, ks)
-
-#=  interconvert: NamedTuple <--> Dict =#
-
-uniontype(nt::NamedTuple) = Union{typeof.(values(nt))...,}
-
-"""
-    gather_(x::Iterable)
-
-Collect the elements of x into a Tuple, in their iterated order.
-"""
-@inline gather_(x::T) where {T} = (collect(x)...,)
-
-if VERSION > v"1.5.9"
- 	namedtuple(d::T) where {T<:AbstractDict{Symbol,V}} where {V} =
-		NamedTuple(d)
-else
-	namedtuple(d::T) where {T<:AbstractDict{Symbol,V}} where {V} =
-	    NamedTuple{gather_(keys(d)), NTuple{length(d), V}}(gather_(values(d)))
-end
-
-namedtuple(d::T) where {T<:AbstractDict{S,V}} where {S<:AbstractString, V} =
-    NamedTuple{Symbol.(gather_(keys(d))), NTuple{length(d), V}}(gather_(values(d)))
-namedtuple(d::T) where {T<:AbstractDict{Symbol,Any}} =
-    NamedTuple{gather_(keys(d)), Tuple{typeof.(values(d))...}}(gather_(values(d)))
-namedtuple(d::T) where {T<:AbstractDict{S,Any}} where {S<:AbstractString} =
-    NamedTuple{Symbol.(gather_(keys(d))), Tuple{typeof.(values(d))...}}(gather_(values(d)))
-
-# use: dict = convert(Dict, nt)
-#=
-   for Dict{Symbol,Any}:
-   Base.convert(::Type{Dict}, x::NT) where {N, NT<:NamedTuple{N}} =
-       Dict([sym=>val for (sym,val) in zip(fieldnames(x), fieldvalues(x))])
-=#
-Base.convert(::Type{D}, x::NT) where {D<:AbstractDict, N, NT<:NamedTuple{N}} =
-    D{Symbol, uniontype(x)}([sym=>val for (sym,val) in zip(fieldnames(x), fieldvalues(x))])
-
-dictionary(nt::NamedTuple) = convert(Dict, nt) # deprecated
-
-# from PR by pdeffebach (Vector of Pairs becomes NamedTuple)
-function namedtuple(v::Vector{<:Pair{<:Symbol}})
-    N = length(v)
-    NamedTuple{ntuple(i -> v[i][1], N)}(ntuple(i -> v[i][2], N))
-end
-# with names as strings
-function namedtuple(v::Vector{<:Pair{String}})
-    N = length(v)
-    NamedTuple{ntuple(i -> Symbol(v[i][1]), N)}(ntuple(i -> v[i][2], N))
-end
-# NamedTuple becomes a Vector of Pairs
-Base.convert(::Type{Vector{Pair}}, nt::NamedTuple) =  map(kv->Pair(first(kv), last(kv)), zip(keys(nt), values(nt)))
-
-# from Sebastian Pfitzner (on Slack)
-macro namedtuple(vars...)
-   args = Any[]
-   for v in vars
-       if Meta.isexpr(v, :(=)) || Meta.isexpr(v, :...)
-           push!(args, esc(v))
-       else
-           push!(args, Expr(:(=), esc(v), esc(v)))
-       end
-   end
-   expr = Expr(:tuple, Expr(:parameters, args...))
-   return expr
-end
-
-end # module NamedTupleTools
+	
+end  # NamedTupleTools
