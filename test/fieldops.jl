f315126fe529adf22c0d55b8c0d2be59ba418f18
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-02 13:47:30 -0500

    Update fieldops.jl

--- test/fieldops.jl
+++ test/fieldops.jl
@@ -5,6 +5,20 @@
    op(::Type{T}) ⟣  fieldcount(T), fieldnames(T), fieldtypes(T)
 =#
 
+@testset "field ops (NamedTuple())" begin
+    T = NamedTuple{(), Tuple{}}
+    t = NamedTuple()
+
+    @test field_count(T)  == 0
+    @test field_names(T)  == ()
+    @test field_types(T)  == ()
+
+    @test field_count(t)  == 0
+    @test field_names(t)  == ()
+    @test field_types(t)  == ()
+    @test field_values(t) == ()
+end
+
 @testset "field ops (NamedTuple)" begin
     T = TestNT
     t = test_nt
@@ -12,6 +26,10 @@
     @test field_count(T)  == 3
     @test field_names(T)  == (:a, :two, :datatype)
     @test field_types(T)  == (Int, Char, UnionAll)
+ 
+    @test field_count(t)  == 3
+    @test field_names(t)  == (:a, :two, :datatype)
+    @test field_types(t)  == (Int, Char, UnionAll)
     @test field_values(t) == (1, '2', NamedTuple)
 end
 
@@ -22,6 +40,10 @@ end
     @test field_count(T)  == 3
     @test field_names(T)  == (:a, :two, :datatype)
     @test field_types(T)  == (Int, Char, DataType)
+
+    @test field_count(t)  == 3
+    @test field_names(t)  == (:a, :two, :datatype)
+    @test field_types(t)  == (Int, Char, UnionAll)
     @test field_values(t) == (1, '2', TestStruct)
 end
 
