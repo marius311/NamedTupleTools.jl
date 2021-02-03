92223bbe83d3e66994d43208155903e73979e142
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-03 15:12:58 -0500

    Update testvalues.jl

--- test/testvalues.jl
+++ test/testvalues.jl
@@ -18,7 +18,7 @@ struct TestStruct
 end
 test_struct = TestStruct(1, '2', TestStruct)
 
-test_ldict = LittleDict(:a=>1, :two=>'2', :datatype=>LittleDict)
+test_ldict = LittleDict((:a, :two, :datatype), (1, '2', LittleDict))
 TestLDict = typeof(test_ldict)
 
 violin_s = (instrument = "violin", madeby = "Stradivari")
