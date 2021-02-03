468c7ed53657439e2714a97b8ba80103f1f5f8c2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-03 13:40:41 -0500

    Update fieldops.jl

--- src/fieldops.jl
+++ src/fieldops.jl
@@ -2,10 +2,10 @@
     Field-based discernment for NamedTuples and structs
 
     field-oriented operations that apply to types with fields
-      op(::Type{T}) ⟣  field_count(T), field_names(T), field_types(T)
+      op(::Type{T}) ⟣  field_count(T), field_names(T), field_types(T), field_tupletype(T)
 
     field-focused operations that apply to instances of types with fields
-      op(x::T)      ⟣  field_count(T), field_names(T), field_types(T)
+      op(x::T)      ⟣  field_count(x), field_names(x), field_types(x), field_tupletype(x)
       op(x::T)      ⟣  field_values(x)
     
     Most programming languages provide functions designed for the processing of values.
@@ -58,6 +58,19 @@ field_types(::Type{NamedTuple{N,T}}) where {N,T} = Tuple(T.parameters)
 field_types(x::DataType)  = fieldtypes(x)
 field_types(x::T) where T = fieldtypes(T)
 field_types(x::LittleDict) = (typeof.(x.vals)...,)
+
+"""
+    field_tupletype
+
+obtains as a Tuple type, the types of the fields
+- works with NamedTuple, struct types
+- works with NamedTuple, struct, LittleDict instances
+""" field_tupletype
+
+field_tupletype(::Type{NamedTuple{N,T}}) where {N,T} = T
+field_tupletype(x::DataType)   = Tuple{fieldtypes(x)...}
+field_tupletype(x::T) where T  = Tuple{fieldtypes(T)...}
+field_tupletype(x::LittleDict) = Tuple{fieldtypes(T)...}
 
 """
     field_values
