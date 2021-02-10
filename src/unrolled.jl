e67d073cdbaedf954709f866bc35cab956b06999
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-09 21:07:29 -0500

    Update unrolled.jl

--- src/unrolled.jl
+++ src/unrolled.jl
@@ -166,30 +166,82 @@ function indexof_unroll(::Val{12}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-# support for tuples of length 13..16
-
 function indexof_unroll(::Val{13}, sym::Symbol, tup::NTuple{N,Symbol}) where N
-    result = indexof_unroll(Val(12), sym, tup)
-    !iszero(result) && return result
-    return 12 + indexof_unroll(Val(1), sym, tup[13:13])
+    (((((((((((((
+    sym === tup[1]  && return  1) ||
+    sym === tup[2]  && return  2) ||
+    sym === tup[3]  && return  3) ||
+    sym === tup[4]  && return  4) ||
+    sym === tup[5]  && return  5) ||
+    sym === tup[6]  && return  6) ||
+    sym === tup[7]  && return  7) ||
+    sym === tup[8]  && return  8) ||
+    sym === tup[9]  && return  9) ||
+    sym === tup[10] && return 10) ||
+    sym === tup[11] && return 11) ||
+    sym === tup[12] && return 12) ||
+    sym === tup[13] && return 13)
+    return 0
 end
 
 function indexof_unroll(::Val{14}, sym::Symbol, tup::NTuple{N,Symbol}) where N
-    result = indexof_unroll(Val(12), sym, tup)
-    !iszero(result) && return result
-    return 12 + indexof_unroll(Val(2), sym, tup[13:14])
+    ((((((((((((((
+    sym === tup[1]  && return  1) ||
+    sym === tup[2]  && return  2) ||
+    sym === tup[3]  && return  3) ||
+    sym === tup[4]  && return  4) ||
+    sym === tup[5]  && return  5) ||
+    sym === tup[6]  && return  6) ||
+    sym === tup[7]  && return  7) ||
+    sym === tup[8]  && return  8) ||
+    sym === tup[9]  && return  9) ||
+    sym === tup[10] && return 10) ||
+    sym === tup[11] && return 11) ||
+    sym === tup[12] && return 12) ||
+    sym === tup[13] && return 13) ||
+    sym === tup[14] && return 14)
+    return 0
 end
 
 function indexof_unroll(::Val{15}, sym::Symbol, tup::NTuple{N,Symbol}) where N
-    result = indexof_unroll(Val(12), sym, tup)
-    !iszero(result) && return result
-    return 12 + indexof_unroll(Val(3), sym, tup[13:15])
+    (((((((((((((((
+    sym === tup[1]  && return  1) ||
+    sym === tup[2]  && return  2) ||
+    sym === tup[3]  && return  3) ||
+    sym === tup[4]  && return  4) ||
+    sym === tup[5]  && return  5) ||
+    sym === tup[6]  && return  6) ||
+    sym === tup[7]  && return  7) ||
+    sym === tup[8]  && return  8) ||
+    sym === tup[9]  && return  9) ||
+    sym === tup[10] && return 10) ||
+    sym === tup[11] && return 11) ||
+    sym === tup[12] && return 12) ||
+    sym === tup[13] && return 13) ||
+    sym === tup[14] && return 14) ||
+    sym === tup[15] && return 15)
+    return 0
 end
 
 function indexof_unroll(::Val{16}, sym::Symbol, tup::NTuple{N,Symbol}) where N
-    result = indexof_unroll(Val(12), sym, tup)
-    !iszero(result) && return result
-    return 12 + indexof_unroll(Val(4), sym, tup[13:16])
+    ((((((((((((((((
+    sym === tup[1]  && return  1) ||
+    sym === tup[2]  && return  2) ||
+    sym === tup[3]  && return  3) ||
+    sym === tup[4]  && return  4) ||
+    sym === tup[5]  && return  5) ||
+    sym === tup[6]  && return  6) ||
+    sym === tup[7]  && return  7) ||
+    sym === tup[8]  && return  8) ||
+    sym === tup[9]  && return  9) ||
+    sym === tup[10] && return 10) ||
+    sym === tup[11] && return 11) ||
+    sym === tup[12] && return 12) ||
+    sym === tup[13] && return 13) ||
+    sym === tup[14] && return 14) ||
+    sym === tup[15] && return 15) ||
+    sym === tup[15] && return 16)
+    return 0
 end
 
 #=
