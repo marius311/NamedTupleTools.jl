d3bfaf5e231148934afe9375ea844662d4328d11
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-17 09:51:06 -0500

    Update inferrables.jl

--- src/inferrables.jl
+++ src/inferrables.jl
@@ -36,4 +36,22 @@ Base.@pure function _fieldnames(@nospecialize t)
 You could try doing the recursion in the type domain, 
     Tuple{:a, :b, :c} using Base.tuple_type_head and Base.tuple_type_tail.
         
-        
+There is also a trick to get this using tuple_type_head and tuple_type_tail
+   from Base. I’m not sure how recommended it is, 
+   but it allows to do inferrable recursive definitions with tuple types. 
+       I’m removing MyType for simplicity.
+
+from_vec(::Type{Tuple{}}, x) = ()
+                
+function from_vec(::Type{T}, x) where {T<:Tuple}
+    S = Base.tuple_type_head(T)
+    T1 = Base.tuple_type_tail(T)
+    return (f(S, x), from_vec(T1, x)...)
+end
+
+# Then, for example,
+
+julia> f(S, x) = S(x)
+
+julia> from_vec(Tuple{Int, Float64}, 1)
+(1, 1.0)        
