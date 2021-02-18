b77337067c37589a1d27c837bc8f4547f91800f2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-17 23:56:55 -0500

    Update construction.jl

--- snippets/construction.jl
+++ snippets/construction.jl
@@ -1,33 +1,31 @@
+ Glossary of Short Forms and Affixes
 
-#=
-   Glossary of Short Forms and Affixes
-   
           NamedT      NamedTuple                NT
-          
+
           NamedTT     NamedTuple Type           NTT
-          
+
           NTNames     NamedTuple Names          (    Symbols... )
           NTTypes     NamedTuple Types          (      Types... )
           NTTYPES     NamedTuple Types Tuple    Tuple{ Types... }
-          
+
           field_name[s]     fieldnames from realized NT
           field_typetuple   Tuple{ fieldtypes... } from realized NT
           field_type[s]     fieldtypes from realized NT
           field_value[s]    fieldvalues from realized NT
-          
+
           NamedTP     NamedTuple Prototype      NTP
           NamedTS     NamedTuple Schema         NTS
-          
+
           NamedTI     NamedTuple Instance       NTI
           NamedTR     NamedTuple Realization    NTR
-          
-          
+
+
    NamedTuple Type      (NTT)
    NamedTuple Prototype (NTP)
    NamedTuple Schema    (NTS)
    NamedTuple Instance  (NTI)
-   
-   The empty NamedTuples are a type and a instance 
+
+   The empty NamedTuples are a type and a instance
    there is a structured NamedTuple type that is empty, it is a singleton
    there is a concrete   NamedTuple type that is empty, it is a singleton
    there is a realized   NamedTuple instance that is empty, it is a singleton
@@ -40,7 +38,7 @@
 typeof( (;) )
 
 # create a NamedTuple with a single valued field
-# 𝗏𝖺𝗋 
+# 𝗏𝖺𝗋
 
 fld𝗉𝗈𝗌₁  = 1               # field's ordinal position
 fld𝗇𝖺𝗆₁  = :letter         # field's name
@@ -63,10 +61,7 @@ semver == (ver = v"2.1")
  :( $fldnam₁ = $fldval₁ )
  :( $fldnam₂ = $fldval₂ )
 
+ NamedTuple{        tupnam₁    }( fldval₁ )
  NamedTuple{      ( fldnam₁ ,) }( fldval₁ )
- NamedTuple{ tuple( fldnam₂  ) }( fldval₂ )
- NamedTuple{ tupn₂  ) }( fldval₂ )
-
- NamedTuple{      ( fldnam₁ ,) }( fldval₁ )
- NamedTuple{ tuple( fldnam₂  ) }( fldval₂ )
+ NamedTuple{ tuple( fldnam₁  ) }( fldval₁ )
 
