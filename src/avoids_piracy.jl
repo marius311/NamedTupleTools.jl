8a9aeb09c333fe58d62f75f3678062986c46f1ef
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-28 08:19:05 -0500

    Update avoids_piracy.jl

--- src/avoids_piracy.jl
+++ src/avoids_piracy.jl
@@ -12,11 +12,20 @@
 tallys the named fields
 """ field_count
 
+field_count(nt::NamedTuple{N,T}) where {N,T} = length(N)
+field_count(nt::Type{NamedTuple{N,T}}) where {N,T} = length(N)
+
 """
     field_names(nt | NT)
 
 provides the field's names as Symbols in a tuple
 """ field_names
+
+"""
+    field_name(nt | NT, position)
+
+provides the name of the field at the position given
+""" field_name
 
 """
     field_types(nt | NT)
@@ -25,10 +34,37 @@ provides the fields' types as a tuple
 """ field_types
 
 """
-    fields_type(nt | NT)
+    field_type(nt | NT, position)
+
+provides the type of the field at the position given
+""" field_type
+
+"""
+    fields_types(nt | NT)
 
 provides the type for the fields as Tuple{ types... }
+""" fields_types
+
+"""
+    fields_type(nt | NT, position)
+
+provides the type for the field at the position given as Tuple{ type }
 """ fields_type
+
+field_names(nt::NamedTuple{N,T}) where {N,T} = N
+field_names(nt::Type{NamedTuple{N,T}}) where {N,T} = N
+field_name(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = N[idx]
+field_name(nt::Type{NamedTuple{N,T}}, idx::Integer) where {N,T} = N[idx]
+
+fields_types(nt::NamedTuple{N,T}) where {N,T} = Tuple{T.parameters...}
+fields_types(nt::Type{NamedTuple{N,T}}) where {N,T} = Tuple{T.parameters...}
+fields_type(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = Tuple{T.parameters[idx]}
+fields_type(nt::Type{NamedTuple{N,T}}, idx::Integer) where {N,T} = Tuple{T.parameters[idx]}
+
+field_types(nt::NamedTuple{N,T}) where {N,T} = Tuple(T.parameters)
+field_types(nt::Type{NamedTuple{N,T}}) where {N,T} = Tuple(T.parameters)
+field_type(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = tuple(T.parameters[idx])
+field_type(nt::Type{NamedTuple{N,T}}, idx::Integer) where {N,T} = tuple(T.parameters[idx])
 
 """
     field_range(nt | NT)
@@ -42,14 +78,6 @@ provides a range for iterating over all fields
 provides the indices for all fields as a sorted tuple
 """ field_indices
 
-field_count(nt::NamedTuple{N,T}) where {N,T} = length(N)
-field_count(nt::Type{NamedTuple{N,T}}) where {N,T} = length(N)
-field_names(nt::NamedTuple{N,T}) where {N,T} = N
-field_names(nt::Type{NamedTuple{N,T}}) where {N,T} = N
-fields_type(nt::NamedTuple{N,T}) where {N,T} = Tuple{T.parameters...}
-fields_type(nt::Type{NamedTuple{N,T}}) where {N,T} = Tuple{T.parameters...} # 8x
-field_types(nt::NamedTuple{N,T}) where {N,T} = Tuple(T.parameters)
-field_types(nt::Type{NamedTuple{N,T}}) where {N,T} = Tuple(T.parameters) # 8x
 field_range(nt::NamedTuple{N,T}) where {N,T} = 1:field_count(nt)
 field_range(nt::Type{NamedTuple{N,T}}) where {N,T} =  1:field_count(nt)
 field_indices(nt::NamedTuple{N,T}) where {N,T}= ntuple(i->i, field_count(nt))
@@ -61,5 +89,12 @@ field_indices(nt::Type{NamedTuple{N,T}}) where {N,T} = ntuple(i->i, field_count(
 the same as `values(nt)`, provided for symmetry
 """ field_values
 
+"""
+    field_value(nt, position)
+
+provides the value of the field at the position given
+""" field_value
+
 field_values(nt::NamedTuple) = values(nt)
+field_value(nt::NamedTuple, idx::Integer) = values(nt)[idx]
 
