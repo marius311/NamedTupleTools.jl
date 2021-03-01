67c414ea723f0cac65af00beedcad17ad0ea218d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-01 15:01:18 -0500

    Update subselections.jl

--- test/subselections.jl
+++ test/subselections.jl
@@ -1,9 +1,27 @@
-@testset "select and omit" begin
+@testset "select" begin
   nt = test_nt
-  @test select(nt, ()) == ()
-  @test select(nt, :event) == (; event = nt.event)
-  @test select(nt, :year)  == (; year = nt.year)
+  names = field_names(nt)
+  firstofnames = names[1]
+  restofnames = Tuple(names[2:end])
   
-  @test select(nt, fieldnames(nt)) == nt
-  @test select(nt, :year, :event)  == (; year = nt.year, event = nt.event)
+  @test select(nt, ()) == (;)
+  @test select(nt, :one) == (; one = nt.one)
+  @test select(nt, :one, :two)  == (; one = nt.one, two = nt.two)
+  
+  @test select(nt, field_names(nt)...) == nt
+  @test select(nt, reverse(field_names(nt))...)  == (; three = nt.three, two = nt.two, one = nt.one)
+end
+
+@testset "omit" begin
+  nt = test_nt
+  names = field_names(nt)
+  firstofnames = names[1]
+  restofnames = Tuple(names[2:end])
+  
+  @test omit(nt, ()) == nt
+  @test omit(nt, :one) == (; two = nt.two, three = nt.three)
+  @test omit(nt, :one, :two)  == (; three = nt.three)
+  
+  @test omit(nt, field_names(nt)...) == (;)
+  @test omit(nt, reverse(field_names(nt))...)  == (;)
 end
