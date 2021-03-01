623a728d47a5f353509d1176b97a52a185a14a8e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-28 23:42:21 -0500

    Update homonyms.jl

--- test/homonyms.jl
+++ test/homonyms.jl
@@ -8,21 +8,22 @@ export field_count,
 
 @testset "field_<method>(::Type{<:NamedTuple})" begin
    NT = Test_NT
-   @test field_count(NT) == fieldcount(NT)
-   @test field_range(NT) == 1:field_count(NT)
+   @test field_count(NT)   == fieldcount(NT)
+   @test field_range(NT)   == 1:field_count(NT)
    @test field_indices(NT) == Tuple(field_range(NT))
-   @test field_names(NT) == fieldnames(NT)
-   @test field_types(NT) == fieldtypes(NT)
-   @test fields_types(NT) == Tuple{fieldtypes(NT)...}
+   @test field_names(NT)   == fieldnames(NT)
+   @test field_types(NT)   == fieldtypes(NT)
+   @test fields_types(NT)  == Tuple{fieldtypes(NT)...}
 end
 
 @testset "field_<method>(indicant::NamedTuple)" begin
+   NT = Test_NT
    nt = test_nt
-   @test field_count(nt) == fieldcount(NT)
-   @test field_range(nt) == 1:field_count(NT)
+   @test field_count(nt)   == fieldcount(NT)
+   @test field_range(nt)   == 1:field_count(NT)
    @test field_indices(nt) == Tuple(field_range(NT))
-   @test field_names(nt) == fieldnames(NT)
-   @test field_types(nt) == fieldtypes(NT)
-   @test fields_types(nt) == Tuple{fieldtypes(NT)...}
-   @test field_values(nt) == values(nt)
+   @test field_names(nt)   == fieldnames(NT)
+   @test field_types(nt)   == fieldtypes(NT)
+   @test fields_types(nt)  == Tuple{fieldtypes(NT)...}
+   @test field_values(nt)  == values(nt)
 end
