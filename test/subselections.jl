bbff09c5abe96892ddc9b3f3036d87de2657f4f5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-01 15:05:45 -0500

    Update subselections.jl

--- test/subselections.jl
+++ test/subselections.jl
@@ -1,27 +1,23 @@
 @testset "select" begin
   nt = test_nt
   names = field_names(nt)
-  firstofnames = names[1]
-  restofnames = Tuple(names[2:end])
   
   @test select(nt, ()) == (;)
   @test select(nt, :one) == (; one = nt.one)
   @test select(nt, :one, :two)  == (; one = nt.one, two = nt.two)
   
-  @test select(nt, field_names(nt)...) == nt
-  @test select(nt, reverse(field_names(nt))...)  == (; three = nt.three, two = nt.two, one = nt.one)
+  @test select(nt, names...) == nt
+  @test select(nt, reverse(names)...)  == (; three = nt.three, two = nt.two, one = nt.one)
 end
 
 @testset "omit" begin
   nt = test_nt
   names = field_names(nt)
-  firstofnames = names[1]
-  restofnames = Tuple(names[2:end])
   
   @test omit(nt, ()) == nt
   @test omit(nt, :one) == (; two = nt.two, three = nt.three)
   @test omit(nt, :one, :two)  == (; three = nt.three)
   
-  @test omit(nt, field_names(nt)...) == (;)
-  @test omit(nt, reverse(field_names(nt))...)  == (;)
+  @test omit(nt, names...) == (;)
+  @test omit(nt, reverse(names)...)  == (;)
 end
