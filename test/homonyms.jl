4ee5b5dc1d12a2a60f659f9f0f0c7b7279be9e4b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-02-28 23:35:09 -0500

    Create homonyms.jl

---
+++ test/homonyms.jl
@@ -0,0 +1,28 @@
+#=
+export field_count, 
+       field_range, field_indices,
+       field_names, field_name, field_types, field_type, 
+       field_values, field_value,
+       destructure, restructure
+=#
+
+@testset "field_<method>(::Type{<:NamedTuple})" begin
+   NT = Test_NT
+   @test field_count(NT) == fieldcount(NT)
+   @test field_range(NT) == 1:field_count(NT)
+   @test field_indices(NT) == Tuple(field_range(NT))
+   @test field_names(NT) == fieldnames(NT)
+   @test field_types(NT) == fieldtypes(NT)
+   @test fields_types(NT) == Tuple{fieldtypes(NT)...}
+end
+
+@testset "field_<method>(indicant::NamedTuple)" begin
+   nt = test_nt
+   @test field_count(nt) == fieldcount(NT)
+   @test field_range(nt) == 1:field_count(NT)
+   @test field_indices(nt) == Tuple(field_range(NT))
+   @test field_names(nt) == fieldnames(NT)
+   @test field_types(nt) == fieldtypes(NT)
+   @test fields_types(nt) == Tuple{fieldtypes(NT)...}
+   @test field_values(nt) == values(nt)
+end
