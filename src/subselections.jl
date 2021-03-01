fb439cfcee19e95bc32fea6f341d9501adef7626
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-01 15:03:18 -0500

    Update subselections.jl

--- src/subselections.jl
+++ src/subselections.jl
@@ -1,7 +1,11 @@
 select(nt::NamedTuple{N,T}, names::Vararg{Symbol}) where {N,T} = 
     NamedTuple{names}(nt)
 
+select(nt::NamedTuple{N,T}, names::Tuple{}) where {N,T} = (;)
+
 function omit(nt::NamedTuple{N,T}, names::Vararg{Symbol}) where {N,T}
     names = Tuple( setdiff(N, names) )
-    NamedTuple{namea}(nt)
+    NamedTuple{names}(nt)
 end
+
+omit(nt::NamedTuple{N,T}, names::Tuple{}) where {N,T} = nt
