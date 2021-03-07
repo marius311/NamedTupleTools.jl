be1f3ade314959b2467850f9b2b5845bd465d26a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-07 04:37:52 -0500

    Update prototype.jl

--- src/prototype.jl
+++ src/prototype.jl
@@ -2,7 +2,8 @@
     prototype(nt | NT, [(types,) | Tuple{types...}]
 
 generate a prototypic NamedTuple `schema` from the given arg[s]
-"""
+""" prototype
+
 prototype(nt::NamedTuple{N,T}) where {N,T} = NamedTuple{N}
 prototype(NT::Type{NamedTuple{N,T}}) where {N,T} = NamedTuple{N}
 
@@ -14,7 +15,8 @@ prototype(nt::NamedTuple{N,T}, types::NTuple{N1,Type}) where {N,T,N1} =
     isprototype( nt | NT )
 
 Predicate that identifies NamedTuple prototypes.
-"""
+""" isprototype
+
 isprototype(::Type{T}) where {T<:NamedTuple} = eltype(T) === Any
 isprototype(nt::T) where {T<:NamedTuple} = false
 isprototype(::Type{UnionAll}) = false
