0a19ea9eb1a2c443da00dacbe37e3b83b2b98fcd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-07 04:28:15 -0500

    Update prototype.jl

--- src/prototype.jl
+++ src/prototype.jl
@@ -1,2 +1,5 @@
 prototype(nt::NamedTuple{N,T}) where {N,T} = NamedTuple{N}
 prototype(NT::Type{NamedTuple{N,T}}) where {N,T} = NamedTuple{N}
+
+prototype(nt::NamedTuple{N,T}, types::NTuple{N1,Type}) where {N,T,N1} =
+    NamedTuple{N,Tuple{types...}}
