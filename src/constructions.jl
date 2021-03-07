fd9c02426a4c487000c036162d8d512296d6ffb5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-07 06:36:37 -0500

    Update constructions.jl

--- src/constructions.jl
+++ src/constructions.jl
@@ -1,3 +1,20 @@
+
+# Expr part from Fredrik Ekre
+structexpr(structname, names, types) =
+	"Expr(:struct,
+		false,
+		Expr(:curly,
+			 :$structname
+		),
+		Expr(:block,
+			map((x,y) -> Expr(:(::), x, y), $names, $types)...
+		)
+	)"
+
+makestruct(structname, names, types) = 
+    (eval(eval(Meta.parse(structexpr(structname, names, types)))); return eval(structname))
+
+
 #=
 answered Mar 27 '20 at 10:00
 François Févotte
