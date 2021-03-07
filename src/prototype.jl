baee386f03eb5ea89b95c7a403afbf410630a9b9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-07 04:33:19 -0500

    Update prototype.jl

--- src/prototype.jl
+++ src/prototype.jl
@@ -3,3 +3,14 @@ prototype(NT::Type{NamedTuple{N,T}}) where {N,T} = NamedTuple{N}
 
 prototype(nt::NamedTuple{N,T}, types::NTuple{N1,Type}) where {N,T,N1} =
     NamedTuple{N,Tuple{types...}}
+
+
+"""
+    isprototype( nt | NT )
+
+Predicate that identifies NamedTuple prototypes.
+"""
+isprototype(::Type{T}) where {T<:NamedTuple} = eltype(T) === Any
+isprototype(nt::T) where {T<:NamedTuple} = false
+isprototype(::Type{UnionAll}) = false
+isprototype(::NamedTuple{(), Tuple{}}) = true
