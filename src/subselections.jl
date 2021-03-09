823ff0cbc1466b7e11239b9a5c742f0a0d665226
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-09 10:07:24 -0500

    Update subselections.jl

--- src/subselections.jl
+++ src/subselections.jl
@@ -10,3 +10,47 @@ end
 
 omit(nt::NamedTuple{N,T}, names::Tuple{}) where {N,T} = nt
 
+
+"""
+    parameters(x)
+
+obtain the parameters attached to x as a SimpleVector
+obtain the kth parameter, reaching in is allowed
+"""
+Base.@pure parameters(x)      = x.parameters
+Base.@pure parameters(x, i)   = x.parameters[i].parameters
+
+Base.@pure parameter(x, i)    = x.parameters[i]
+Base.@pure parameter(x, i, j) = x.parameters[i].parameters[j]
+
+getindicies(x, idxs::NTuple{N,Integer}) where N = getindex.(tuple(x), idxs)
+
+"""
+    enTuple(x)
+
+wrap the type `Tuple` about x[s] obtaining Tuple{x}, Tuple{xs...}
+"""
+enTuple(x) = Tuple{x}
+enTuple(x::Tuple) = Tuple{x...}
+enTuple(x::Vararg{Type}) = Tuple{x...}
+
+"""
+    unTuple(x)
+
+dual to
+"""
+
+"""
+    untuple( Tuple{_} )
+
+Retrieve the types that are internal to the `Type{Tuple{_}}` as a `(tuple,)`.
+"""
+untuple(::Type{T}) where {T<:Tuple} = (T.parameters...,)
+untuple(::Type{T}) where {T<:Tuple{Vararg{Any}}} = T.parameters
+
+"""
+    retuple( (_) )
+
+Generate a `Type{Tuple{_}}` with the given internal types as a `Tuple{_}`.
+"""
+retuple(x::Tuple) = Tuple{x...,}
