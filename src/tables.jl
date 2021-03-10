222acb04fdec559fc7c3dc4151865351371b9870
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-10 10:22:51 -0500

    Update tables.jl

--- src/tables.jl
+++ src/tables.jl
@@ -2,3 +2,15 @@ using Tables, DataFrames
 
 # Tables.namedtupleiterator
 # t's very easy to turn the dataframe into a named tuple via Tables.columntable
+
+randresult = rand(NamedTuple,6,8,3,permutevals=true,permutesyms=true)
+
+ntstack = []
+for r in randresult
+  push!(ntstack, r)
+end
+
+# in this order
+tableofrows = Tables.rowtable(ntstack)
+tableofcols = Tables.columntable(tableofrows)
+
