328944f2209bbfe0ae62822cd4691cb991a3b9fb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-10 20:08:04 -0500

    Update tables.jl

--- src/tables.jl
+++ src/tables.jl
@@ -10,7 +10,24 @@ for r in randresult
   push!(ntstack, r)
 end
 
-# in this order
-tableofrows = Tables.rowtable(ntstack)
-tableofcols = Tables.columntable(tableofrows)
+#=
+   There are two common ways to store tabulated information.
+   (a) "column major order", where one column follows another
+        - adjacent columns are closer than adjacent rows
+   (b) "row major order", where one row follows another
+        - adjacent rows are closer than adjacent columns
+   Julia follows 
+
+meet where one ends and the next begins)
+
+are contigous
+   row-major (faster when accessed as a sequence of rows)
+# which data storage orientation is easier for your pkg's clients?
+# which data storage orientation is more intuitive, and which is more performant? 
+
+orientation, column dominant or row dominantis easier for your users table organization is easier for your users
+# which table r is more performant for your user
+table_of_rows    = Tables.rowtable(ntstack)
+table_of_columns = Tables.columntable(tableofrows)
+
 
