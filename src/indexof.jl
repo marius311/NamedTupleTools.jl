12252bfb2927118afc9379385050817c8a995b7a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-18 20:44:30 -0400

    Update indexof.jl

--- src/indexof.jl
+++ src/indexof.jl
@@ -1,26 +1,75 @@
+
+#=
+   forall xs | 0  <= length(xs) <= 16 the fastest hand unrolled functions are used
+   forall xs | 17 <= length(xs) <= 32 the very fast generated functions are used
+   forall xs | 33 <= length(xs)       
+=#
+
+function indexof(s::Symbol, tup::NTuple{N, Symbol}) where {N}
+    if N < 17
+        index_of(Val(N), s, tup)
+    else
+        index_of(s, tup)
+    end
+end
+
+# generated function
+
+@generated function index_of(s::Symbol, tup::NTuple{N, Symbol}) where {N}
+    ex = :(s === tup[1] && return 1)
+        foreach(2:N) do i
+            ex = :($ex || (s === tup[$i] && return $i))
+        end
+    quote
+        $ex
+        0
+    end
+end
+
+@generated function index_of(s::Symbol, tup1::NTuple{N1, Symbol}, tup2::NTuple{N2, Symbol}) where {N1, N2}
+    ex = :(s === tup1[1] && return 1)
+       foreach(2:N1) do i
+           ex = :($ex || (s === tup1[$i] && return $i))
+       end
+       foreach(1:N2) do i
+           ex = :($ex || (s === tup2[$i] && return $i+N1))
+       end
+    quote
+       $ex
+       0
+    end
+end
+
+
+# hand unrolled functions
+
 """
-    indexof(Val(N), x::Symbol, xs::NTuple{N,Symbol}) where N
+    index_of(Val(N), x::Symbol, xs::NTuple{N,Symbol}) where N
 
 nothing, if x does not occur in xs, otherwise
 idx, where idx is the first (smallest) index where xs[idx] == x
 
 for tuples of 16 or fewer elements, positionof is hand unrolled
-""" indexof
+""" index_of
 
-function indexof(::Val{1}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{0}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+    return 0
+end
+
+function index_of(::Val{1}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     (
     sym === tup[1] && return 1)
     return 0
 end
 
-function indexof(::Val{2}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{2}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     ((
     sym === tup[1] && return 1) ||
     sym === tup[2] && return 2)
     return 0
 end
 
-function indexof(::Val{3}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{3}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     (((
     sym === tup[1] && return 1) ||
     sym === tup[2] && return 2) ||
@@ -28,7 +77,7 @@ function indexof(::Val{3}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{4}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{4}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     ((((
     sym === tup[1] && return 1) ||
     sym === tup[2] && return 2) ||
@@ -37,7 +86,7 @@ function indexof(::Val{4}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{5}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{5}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     (((((
     sym === tup[1] && return 1) ||
     sym === tup[2] && return 2) ||
@@ -47,7 +96,7 @@ function indexof(::Val{5}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{6}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{6}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     ((((((
     sym === tup[1] && return 1) ||
     sym === tup[2] && return 2) ||
@@ -58,7 +107,7 @@ function indexof(::Val{6}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{7}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{7}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     (((((((
     sym === tup[1] && return 1) ||
     sym === tup[2] && return 2) ||
@@ -70,7 +119,7 @@ function indexof(::Val{7}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{8}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{8}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     ((((((((
     sym === tup[1] && return 1) ||
     sym === tup[2] && return 2) ||
@@ -83,7 +132,7 @@ function indexof(::Val{8}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{9}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{9}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     (((((((((
     sym === tup[1] && return 1) ||
     sym === tup[2] && return 2) ||
@@ -97,7 +146,7 @@ function indexof(::Val{9}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{10}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{10}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     ((((((((((
     sym === tup[1]  && return  1) ||
     sym === tup[2]  && return  2) ||
@@ -112,7 +161,7 @@ function indexof(::Val{10}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{11}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{11}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     (((((((((((
     sym === tup[1]  && return  1) ||
     sym === tup[2]  && return  2) ||
@@ -128,7 +177,7 @@ function indexof(::Val{11}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{12}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{12}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     ((((((((((((
     sym === tup[1]  && return  1) ||
     sym === tup[2]  && return  2) ||
@@ -145,7 +194,7 @@ function indexof(::Val{12}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{13}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{13}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     (((((((((((((
     sym === tup[1]  && return  1) ||
     sym === tup[2]  && return  2) ||
@@ -163,7 +212,7 @@ function indexof(::Val{13}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{14}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{14}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     ((((((((((((((
     sym === tup[1]  && return  1) ||
     sym === tup[2]  && return  2) ||
@@ -182,7 +231,7 @@ function indexof(::Val{14}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{15}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{15}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     (((((((((((((((
     sym === tup[1]  && return  1) ||
     sym === tup[2]  && return  2) ||
@@ -202,7 +251,7 @@ function indexof(::Val{15}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     return 0
 end
 
-function indexof(::Val{16}, sym::Symbol, tup::NTuple{N,Symbol}) where N
+function index_of(::Val{16}, sym::Symbol, tup::NTuple{N,Symbol}) where N
     ((((((((((((((((
     sym === tup[1]  && return  1) ||
     sym === tup[2]  && return  2) ||
