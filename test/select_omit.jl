1994288c242f1675494eba1742c01d4976419ec2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-27 01:45:18 -0400

    Update select_omit.jl

--- test/select_omit.jl
+++ test/select_omit.jl
@@ -3,18 +3,42 @@
   nt_names = field_names(nt)
   
   @test select(nt, ()) == (;)
+
+  @test select(nt, (:one,)) == (; one = nt.one)
+  @test select(nt, (:one, :two,))  == (; one = nt.one, two = nt.two)
+  @test select(nt, nt_names) == nt
+  @test select(nt, reverse(nt_names))  == (; three = nt.three, two = nt.two, one = nt.one)
+
   @test select(nt, :one) == (; one = nt.one)
   @test select(nt, :one, :two)  == (; one = nt.one, two = nt.two)
-  
   @test select(nt, nt_names...) == nt
   @test select(nt, reverse(nt_names)...)  == (; three = nt.three, two = nt.two, one = nt.one)
-end
+en
 
 @testset "omit" begin
   nt = test_nt
   nt_names = field_names(nt)
   
   @test omit(nt, ()) == nt
+  
+  @test omit(nt, (:one,)) == (; two = nt.two, three = nt.three)
+  @test omit(nt, (:two,)) == (; one = nt.one, three = nt.three)
+  @test omit(nt, (:three,)) == (; one = nt.one, two = nt.two)
+  @test omit(nt, (:one, :one)) == (; two = nt.two, three = nt.three)
+  @test omit(nt, (:two, :two, :two)) == (; one = nt.one, three = nt.three)
+
+  @test omit(nt, (:one, :two,))  == (; three = nt.three)
+  @test omit(nt, (:two, :one,))  == (; three = nt.three)
+  @test omit(nt, (:one, :three,))  == (; two = nt.two)
+  @test omit(nt, (:three, :one,))  == (; two = nt.two)
+  @test omit(nt, (:two, :three,))  == (; one = nt.one)
+  @test omit(nt, (:three, :two,))  == (; one = nt.one)
+    
+  @test omit(nt, (:one, :two, :three,))  == (;)
+  @test omit(nt, (:one, :three, :two,))  == (;)
+  @test omit(nt, (:two, :three, :one,))  == (;)
+  @test omit(nt, (:three, :two, :one))  == (;)
+    
   @test omit(nt, :one) == (; two = nt.two, three = nt.three)
   @test omit(nt, :one, :two)  == (; three = nt.three)
   
