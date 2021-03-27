58f2e1c057c96b61578097091002abb0dedcada1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-27 01:34:54 -0400

    Update select_omit.jl

--- test/select_omit.jl
+++ test/select_omit.jl
@@ -1,23 +1,23 @@
 @testset "select" begin
   nt = test_nt
-  names = field_names(nt)
+  nt_names = field_names(nt)
   
   @test select(nt, ()) == (;)
   @test select(nt, :one) == (; one = nt.one)
   @test select(nt, :one, :two)  == (; one = nt.one, two = nt.two)
   
-  @test select(nt, names...) == nt
-  @test select(nt, reverse(names)...)  == (; three = nt.three, two = nt.two, one = nt.one)
+  @test select(nt, nt_names...) == nt
+  @test select(nt, reverse(nt_names)...)  == (; three = nt.three, two = nt.two, one = nt.one)
 end
 
 @testset "omit" begin
   nt = test_nt
-  names = field_names(nt)
+  nt_names = field_names(nt)
   
   @test omit(nt, ()) == nt
   @test omit(nt, :one) == (; two = nt.two, three = nt.three)
   @test omit(nt, :one, :two)  == (; three = nt.three)
   
-  @test omit(nt, names...) == (;)
-  @test omit(nt, reverse(names)...)  == (;)
+  @test omit(nt, nt_names...) == (;)
+  @test omit(nt, reverse(nt_names)...)  == (;)
 end
