a88760928af910ab0ecca7a865e3dc0bacd43c46
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-27 00:47:56 -0400

    Update select_omit.jl

--- benchmark/select_omit.jl
+++ benchmark/select_omit.jl
@@ -1,3 +1,7 @@
 # Benchmarking `select(nt|NT, names)` and `omit(nt|NT, names)`
 
+using BenchmarkTools; 
+const BTparam = BenchmarkTools.DEFAULT_PARAMETERS; 
+BTparam.samples = 50_000; BTparam.time_tolerance = 1/64;
+
 include("../test/testvalues.jl")
