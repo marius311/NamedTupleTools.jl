054334f6fac0037249d71dd8391fa6a9101cc5fa
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-03-27 01:51:03 -0400

    Update select_omit.jl

--- src/select_omit.jl
+++ src/select_omit.jl
@@ -47,19 +47,21 @@ julia> map(s->!any(s in hay), needles)
 =#
 
 # @inline not_occurs_in(needles, hay) = hay[[foldl(.&,map(.!,[n .== hay for n in needles]))...]]
-@inline not_occurs_in(needles, hay) = filter(needle -> !(needle in hay), needles)
+@inline not_occurs_in(needles, hay) = filter(straw -> !(straw in needles), hay)
 
-function omit(nt::NamedTuple{N,T}, names::Tuple{Vararg{Symbol}}) where {N,T}
-    unnamed = not_occurs_in(names, N)
-    NamedTuple{unnamed}(nt)
+function omit(nt::NamedTuple{N,T}, omit_names::Tuple{Vararg{Symbol}}) where {N,T}
+    keep_names = not_occurs_in(omit_names, N)
+    NamedTuple{keep_names}(nt)
 end
+
+omit(nt::NamedTuple{N,T}, omit_names::Tuple{}) where {N,T} = nt
+
 #=
 function omit(nt::NamedTuple{N,T}, names::Vararg{Symbol}) where {N,T}
     names = Tuple( setdiff(N, names) )
     NamedTuple{names}(nt)
 end
 =#
-omit(nt::NamedTuple{N,T}, names::Tuple{}) where {N,T} = nt
 
 #=
 """
