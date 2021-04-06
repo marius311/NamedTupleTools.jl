807ed1b24a9d154f406a550e4a185aea65326396
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-04-05 22:30:23 -0400

    Update namedtupletype.jl

--- src/namedtupletype.jl
+++ src/namedtupletype.jl
@@ -27,7 +27,16 @@ ntt(names::Vararg{<:AbstractString}) = ntt(Symbol.(names))
 ntt(names::Vector{Symbol}) = ntt(Tuple(names))
 ntt(names::Vector{<:AbstractString}) = ntt(Symbol.(names))
 
-ntt(name::Symbol, type::Type) = NamedTuple{(name,), Tuple{type}}
-ntt(name::Tuple{Symbol}, type::Tuple{DataType}) = ntt(name[1], type[1])      
+ntt(name::Symbol, type::DataType) = NamedTuple{(name,), Tuple{type}}
 ntt(name::Symbol, type::Tuple{DataType}) = NamedTuple{(name,), Tuple{type[1]}}
-ntt(name::Tuple{Symbol}, type::DataType) = NamedTuple{name, Tuple{type})      
+ntt(name::Tuple{Symbol}, type::DataType) = NamedTuple{name, Tuple{type}}      
+ntt(name::Tuple{Symbol}, type::Tuple{DataType}) = ntt(name, Tuple{type[1]})
+                                       
+ntt(name::Symbol, type::Vector{<:DataType}) = NamedTuple{(name,), Tuple{type[1]}}
+ntt(names::NTuple{N,Symbol}, types::Vector{<:DataType}) where {N} = NamedTuple{names, Tuple{types[1:N]...}}     
+ntt(names::Vector{Symbol}, types::NTuple{N, <:DataType}) where {N} = NamedTuple{Tuple(names[1:N]), Tuple{types...}}
+ntt(names::Vector{Symbol}, types::Vector{<:DataType}) = let N=min(length(names), length(types))
+    NamedTuple{Tuple(names[1:N], Tuple{types[1:N]...}}
+end
+              
+
