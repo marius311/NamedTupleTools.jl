121b85d68561d6938dc9ef7f464164a060b6807f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-04-05 22:48:21 -0400

    Update namedtupletype.jl

--- src/namedtupletype.jl
+++ src/namedtupletype.jl
@@ -55,14 +55,14 @@ using Test
 @test ntt() == NamedTuple{(), Tuple{}}
 @test ntt(:a) == NamedTuple{(:a,)}
 @test ntt("a") == NamedTuple{(:a,)}
-@test ntt(:a, :b) = NamedTuple{(:a, :b)}
-@test ntt("a", "b") = NamedTuple{(:a, :b)}
+@test ntt(:a, :b) == NamedTuple{(:a, :b)}
+@test ntt("a", "b") == NamedTuple{(:a, :b)}
 @test ntt((:a,)) == NamedTuple{(:a,)}
 @test ntt(("a",)) == NamedTuple{(:a,)}
-@test ntt((:a, :b)) = NamedTuple{(:a, :b)}
-@test ntt(("a", "b")) = NamedTuple{(:a, :b)}
-@test ntt([:a, :b]) = NamedTuple{(:a, :b)}
-@test ntt(["a", "b"]) = NamedTuple{(:a, :b)}
+@test ntt((:a, :b)) == NamedTuple{(:a, :b)}
+@test ntt(("a", "b")) == NamedTuple{(:a, :b)}
+@test ntt([:a, :b]) == NamedTuple{(:a, :b)}
+@test ntt(["a", "b"]) == NamedTuple{(:a, :b)}
 
 @test ntt(:a, Int) == NamedTuple{(:a,), Tuple{Int}}
 @test ntt("a", Int) == NamedTuple{(:a,), Tuple{Int}}
@@ -72,17 +72,17 @@ using Test
 @test ntt(("a",), Int) == NamedTuple{(:a,), Tuple{Int}}
 @test ntt((:a,), (Int,)) == NamedTuple{(:a,), Tuple{Int}}
 @test ntt(("a",), (Int,)) == NamedTuple{(:a,), Tuple{Int}}
-@test ntt((:a, :b), (Int, String)) = NamedTuple{(:a, :b), Tuple{Int, String}}
-@test ntt(("a", "b"), (Int, String)) = NamedTuple{(:a, :b), Tuple{Int, String}}
-@test ntt([:a, :b], (Int, String)) = NamedTuple{(:a, :b), Tuple{Int, String}}
-@test ntt(["a", "b"], (Int, String)) = NamedTuple{(:a, :b), Tuple{Int, String}}
+@test ntt((:a, :b), (Int, String)) == NamedTuple{(:a, :b), Tuple{Int, String}}
+@test ntt(("a", "b"), (Int, String)) == NamedTuple{(:a, :b), Tuple{Int, String}}
+@test ntt([:a, :b], (Int, String)) == NamedTuple{(:a, :b), Tuple{Int, String}}
+@test ntt(["a", "b"], (Int, String)) == NamedTuple{(:a, :b), Tuple{Int, String}}
                                                                                                                   
 @test ntt((:a,), [Int,]) == NamedTuple{(:a,), Tuple{Int}}
 @test ntt(("a",), [Int,]) == NamedTuple{(:a,), Tuple{Int}}
-@test ntt((:a, :b), [Int, String]) = NamedTuple{(:a, :b), Tuple{Int, String}}
-@test ntt(("a", "b"), [Int, String]) = NamedTuple{(:a, :b), Tuple{Int, String}}
-@test ntt([:a, :b], [Int, String]) = NamedTuple{(:a, :b), Tuple{Int, String}}
-@test ntt(["a", "b"], [Int, String]) = NamedTuple{(:a, :b), Tuple{Int, String}}
+@test ntt((:a, :b), [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
+@test ntt(("a", "b"), [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
+@test ntt([:a, :b], [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
+@test ntt(["a", "b"], [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
                                                                                                            
                                                                                                                   
                                                                                                                   
