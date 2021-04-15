af08d68ebf8ae40a5abcee4c17d55a33b129df84
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-04-15 16:18:46 -0400

    Update fields.jl

--- src/fields.jl
+++ src/fields.jl
@@ -1,3 +1,16 @@
+field_count(ntt::Type{NamedTuple{N,T}}) where {N,T} = nfields(N)
+field_count(nt::NamedTuple{N,T}) where {N,T} = nfields(N)
+
+field_names(ntt::Type{NamedTuple{N,T}}) where {N,T} = ntt.parameters[1]
+field_names(nt::NamedTuple{N,T}) where {N,T} = N
+
+field_types(ntt::Type{NamedTuple{N,T}}) where {N,T} = ntt.parameters[2]
+field_types(nt::NamedTuple{N,T}) where {N,T} = T
+
+field_values(nt::NamedTuple{N,T}) where {N,T} = values(nt)
+
+
+
 #=
     Field-based discernment for NamedTuples and structs
     field-oriented operations that apply to types with fields
