4020a221cb69cb3ce493c9649be9e90afc0bf842
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-04-16 06:02:24 -0400

    Update fieldops.jl

--- src/fieldops.jl
+++ src/fieldops.jl
@@ -12,3 +12,11 @@ field_tupletypes(nt::NamedTuple{N,T}) where {N,T} = T
 field_types(nt::NamedTuple{N,T}) where {N,T} = Tuple(T.parameters)
 field_values(nt::NamedTuple{N,T}) where {N,T} = values(nt)
 
+field_names(ntt::Type{NamedTuple{N}}, idx::Integer) where {N} = N[idx]
+field_names(ntt::Type{NamedTuple{N,T}}, idx::Integer) where {N,T} = N[idx]
+field_types(ntt::Type{NamedTuple{N,T}}, idx::Integer) where {N,T} = T.parameters[idx]
+
+field_names(nt::NamedTuple{N}, idx::Integer) where {N} = N[idx]
+field_names(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = N[idx]
+field_types(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = T.parameters[idx]
+field_values(nt::NamedTuple{N}, idx::Integer) where {N} = values(nt)[idx]
