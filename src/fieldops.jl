764d906de13e9c820ffc86f0514cf2d0ab66314a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-04-16 06:07:40 -0400

    Update fieldops.jl

--- src/fieldops.jl
+++ src/fieldops.jl
@@ -20,3 +20,12 @@ field_names(nt::NamedTuple{N}, idx::Integer) where {N} = N[idx]
 field_names(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = N[idx]
 field_types(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = T.parameters[idx]
 field_values(nt::NamedTuple{N}, idx::Integer) where {N} = values(nt)[idx]
+
+field_names(ntt::Type{NamedTuple{N}}, idxs::Vararg{<:Integer}) where {N} = map(i->field_names(ntt,i), idxs)
+field_names(ntt::Type{NamedTuple{N,T}}, idxs::Vararg{<:Integer}) where {N,T} = map(i->field_names(ntt,i), idxs)
+field_types(ntt::Type{NamedTuple{N,T}}, idxs::Vararg{<:Integer}) where {N,T} = map(i->field_types(ntt,i), idxs)
+
+field_names(nt::NamedTuple{N}, idxs::Vararg{<:Integer}) where {N} = map(i->field_names(nt,i), idxs)
+field_names(nt::NamedTuple{N,T}, idxs::Vararg{<:Integer}) where {N,T} = map(i->field_names(nt,i), idxs)
+field_types(nt::NamedTuple{N,T}, idxs::Vararg{<:Integer}) where {N,T} = map(i->field_types(nt,i), idxs)
+field_values(nt::NamedTuple{N}, idxs::Vararg{<:Integer}) where {N} = map(i->field_values(ntt,i), idxs)
