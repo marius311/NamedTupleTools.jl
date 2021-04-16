afb6f733e20eb3d8e498c3cc0d66eeabb8d66838
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-04-15 20:14:10 -0400

    Create decompose_parts.jl

---
+++ src/decompose_parts.jl
@@ -0,0 +1,22 @@
+# field selected field_items
+
+field_count(ntt::Type{NamedTuple{N,T}}) where {N,T} = nfields(N)
+field_count(nt::NamedTuple{N,T}) where {N,T} = nfields(N)
+
+field_names(ntt::Type{NamedTuple{N,T}}) where {N,T} = ntt.parameters[1]
+field_names(nt::NamedTuple{N,T}) where {N,T} = N
+
+field_types(ntt::Type{NamedTuple{N,T}}) where {N,T} = ntt.parameters[2]
+field_types(nt::NamedTuple{N,T}) where {N,T} = T
+
+field_values(nt::NamedTuple{N,T}) where {N,T} = values(nt)
+
+# (field, index) selected field_item
+
+field_name(ntt::Type{NamedTuple{N,T}}, idx::Integer) where {N,T} = getfield(N, idx)
+field_name(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = getfield(N, idx)
+
+field_type(ntt::Type{NamedTuple{N,T}}, idx::Integer) where {N,T} = getfield(T, idx)
+field_type(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = (T.parameters)[idx]
+
+field_value(nt::NamedTuple{N,T}, idx::Integer) where {N,T} = getfield(nt, idx)
