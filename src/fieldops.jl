fcf6b967459489586856df373eb1b26688696748
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-04-22 22:17:06 -0400

    Update fieldops.jl

--- src/fieldops.jl
+++ src/fieldops.jl
@@ -1,13 +1,34 @@
-field_count(ntt::Type{NamedTuple{N}}) where {N} = nfields(N)
-field_names(ntt::Type{NamedTuple{N}}) where {N} = N
 
-field_count(ntt::Type{NamedTuple{N,T}}) where {N,T} = nfields(N)
-field_names(ntt::Type{NamedTuple{N,T}}) where {N,T} = N
+"""
+    field_count(x|T)
+count the fields specified with 'T' or present in 'x::T'
+works with these Types and their instances
+- NamedTuples, DataTypes (structs), Tuples
+""" field_count
+
+field_count(NT::Type{NamedTuple{N}}) where {N} = nfields(N)
+field_count(NT::Type{NamedTuple{N,T}}) where {N,T} = nfields(N)
+field_count(nt::NamedTuple{N,T}) where {N,T} = nfields(N)
+field_count(x::Type{T}) where {T} = fieldcount(x)
+field_count(x::T) where {T} = fieldcount(T)
+
+"""
+    field_names(::T|T)
+obtain the names of fields specified with 'T' or present in 'x::T'
+works with these Types and their instances
+- NamedTuples, DataTypes (structs)
+""" field_names
+
+field_names(NT::Type{NamedTuple{N}}) where {N} = N
+field_names(NT::Type{NamedTuple{N,T}}) where {N,T} = N
+field_names(nt::NamedTuple{N,T}) where {N,T} = N
+field_names(x::T) where {T<:DataType} = fieldnames(x)
+field_names(x::T) where {T} = field_names(T)
+
+
 field_tupletypes(ntt::Type{NamedTuple{N,T}}) where {N,T} = T
 field_types(ntt::Type{NamedTuple{N,T}}) where {N,T} = Tuple(T.parameters)
 
-field_count(nt::NamedTuple{N,T}) where {N,T} = nfields(N)
-field_names(nt::NamedTuple{N,T}) where {N,T} = N
 field_tupletypes(nt::NamedTuple{N,T}) where {N,T} = T
 field_types(nt::NamedTuple{N,T}) where {N,T} = Tuple(T.parameters)
 field_values(nt::NamedTuple{N,T}) where {N,T} = values(nt)
