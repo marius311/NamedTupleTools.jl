2977906e26b6657c8194be8ed2be4aa88c70acab
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-05-17 03:07:50 -0400

    Update NamedTupleTools.jl

--- src/NamedTupleTools.jl
+++ src/NamedTupleTools.jl
@@ -1,6 +1,21 @@
 module NamedTupleTools
 
-export namedtuple
+export # familiar field operations (piracy avoiding)
+       field_count, 
+       field_names,  field_name,
+       field_types,  field_type,
+       field_values, field_value,
+       # data structure interconversions
+       namedtuple_from,
+       tuple_from, dict_from, struct_from,
+       # editing: include, exclude,
+       reposition_fields, reposition_field,
+       remove_from,
+       prepend_fields,  prepend_field,
+       postpend_fields, postpend_field,
+
+
+namedtuple
 
 include("field_ops.jl")
 
