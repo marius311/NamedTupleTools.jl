f3adb3c554167edfa49af2f25340e59a9b0d0f4a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-05-17 06:05:04 -0400

    Update and rename runtests0.jl to runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,16 @@
+using NamedTupleTools
+using OrderedCollections: OrderedDict, LittleDict
+using Test
+
+#=
+    types and realized values
+    for use with testing only
+
+                    | Type       | value
+        ------------|------------|------------
+        NamedTuple  | TestNT     | test_nt
+        struct      | TestStruct | test_struct   
+=#
+include("testvalues.jl")
+
+include("field_ops.jl")
