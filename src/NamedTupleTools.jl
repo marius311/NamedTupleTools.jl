afd9cde50aa9f41661dd8cb25c23c68cf4686964
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-04 15:52:29 -0500

    Update NamedTupleTools.jl

--- src/NamedTupleTools.jl
+++ src/NamedTupleTools.jl
@@ -1,11 +1,11 @@
 module NamedTupleTools
 
 export # familiar field operations, expanded applicability      (avoids piracy)
-       field_count, field_names,     # all  NamedTupleTypes, all NamedTuples
-       field_types,                  # some NamedTupleTypes, all NamedTuples
-       field_values,                 # none NamedTupleTypes, all NamedTuples
+       field_count, field_names,       # all  NamedTupleTypes, all NamedTuples
+       field_types, field_tupletypes,  # some NamedTupleTypes, all NamedTuples
+       field_values,                   # none NamedTupleTypes, all NamedTuples
        # data structure interconversions, args follow `convert` (avoids piracy)
-       restructure,                  # restructure(::Type{Target}, x::Source)
+       restructure,                    # restructure(::Type{Target}, x::Source)
        # editing: include, exclude,
        reposition_fields, reposition_field,
        remove_from,
