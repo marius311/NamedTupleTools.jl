ce167c9ace6f57bcba1f6aa7bfe8becf23fba426
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-04 09:52:34 -0500

    Create restructure.jl

---
+++ src/restructure.jl
@@ -0,0 +1,8 @@
+#=
+    `restructure` is intended to be a generic method for interconverting data structures (without type piracy).
+    - args follow `convert`:  restructure(::Type{Target}, x::Source)
+
+    This file collects conversions that facilitate working with NamedTuples.
+=#
+
+
