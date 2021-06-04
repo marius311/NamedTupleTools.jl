4bbfe3f507ac46eddcc9a2e914fa771152b8f0e9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-04 09:41:10 -0500

    Update support.jl

--- src/support.jl
+++ src/support.jl
@@ -1,2 +1,22 @@
-getindices(x, idx::Integer) = getindex(x, idx)
-getindices(x, idxs::NTuple{L,T}) where {L, T<:Integer} = Tuple(map(i->getindex(x, i), idxs))
+"""
+    getindices(x::NamedTuple, <indices>)
+
+`getindices` extends and is compatible with `Base.getindex` for `NamedTuples`.
+
+- Integer indices are ordinal positions within `x`.
+- Symbol indices are field names appearing in `x`.
+
+(a) A single position or a single field name may be used for <indices>.
+In these cases `getindices` follows `getindex`, returning a value.
+
+(b) A `Tuple` of field names may be used for <indices>.
+In this case `getindicies` follows `getindex`, returning a `NamedTuple`.
+
+(c) A `Tuple` of positions may be used for <indices>.
+In this case `getindicies` extends `getindex`, returning a `NamedTuple`.
+"""
+
+@nospecialize getindices(@nospecialize x, @nospecialize idx::Union{Int32, Int64, Symbol}) = getindex(x, idx)
+getindices(@nospecialize x, @nospecialize idx::Integer) = getindex(x, idx)
+getindices(@nospecialize x, @nospecialize idxs::NTuple{L,T}) where {L, T<:Integer} = Tuple(map(i->getindex(x, i), idxs))
+getindices(@nospecialize x, @nospecialize idxs::NTuple{L,Symbol}) where {L} = getindex(x, idxs)
