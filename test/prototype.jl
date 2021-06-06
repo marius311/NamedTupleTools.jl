84e5ae338a0fc375a1bdb5c7e36d3dbbef19956b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-06 14:36:30 -0500

    Update prototype.jl

--- test/prototype.jl
+++ test/prototype.jl
@@ -18,6 +18,8 @@ speicifying types work with any length-matched tuple of values.
 
 =#
 
+# test prototype construction from wholes
+
 @testset "prototype(_)" begin
   @test prototype(Test_NTP) == Test_NTP
   @test prototype(Test_NTT) == Test_NTP
@@ -37,5 +39,59 @@ end
   @test prototype(test_nt; types=true)  == Test_NTT
 end
 
+# test prototype construction from <empty>
+
+@test prototype() == NamedTuple{(), Tuple{}}
+
+# test prototype construction from parts with symbols
+
+@testset "prototypes from symbols" begin
+  @test prototype(:a) == NamedTuple{(:a,)}
+  @test prototype(:a, :b) == NamedTuple{(:a, :b)}
+  @test prototype((:a,)) == NamedTuple{(:a,)}
+  @test prototype((:a, :b)) == NamedTuple{(:a, :b)}
+  @test prototype([:a, :b]) == NamedTuple{(:a, :b)}
+end
+
+@testset "prototypes from symbols and types" begin
+  @test prototype(:a, Int) == NamedTuple{(:a,), Tuple{Int}}
+  @test prototype(:a, (Int,)) == NamedTuple{(:a,), Tuple{Int}}
+  @test prototype((:a,), Int) == NamedTuple{(:a,), Tuple{Int}}
+
+  @test prototype((:a,), (Int,)) == NamedTuple{(:a,), Tuple{Int}}
+  @test prototype((:a, :b), (Int, String)) == NamedTuple{(:a, :b), Tuple{Int, String}}
+  @test prototype([:a, :b], (Int, String)) == NamedTuple{(:a, :b), Tuple{Int, String}}
+
+  @test prototype((:a,), [Int,]) == NamedTuple{(:a,), Tuple{Int}}
+  @test prototype((:a, :b), [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
+  @test prototype([:a, :b], [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
+end
+
+# test prototype construction from parts with strings
+
+@testset "prototypes from strings" begin
+  @test prototype("a") == NamedTuple{(:a,)}
+  @test prototype("a", "b") == NamedTuple{(:a, :b)}
+  @test prototype(("a",)) == NamedTuple{(:a,)}
+  @test prototype(("a", "b")) == NamedTuple{(:a, :b)}
+  @test prototype(["a", "b"]) == NamedTuple{(:a, :b)}
+end
+
+# test prototype construction from parts with strings and types
+
+@testset "prototypes from strings and types" begin
+  @test prototype("a", Int) == NamedTuple{(:a,), Tuple{Int}}
+  @test prototype("a", (Int,)) == NamedTuple{(:a,), Tuple{Int}}
+  @test prototype(("a",), Int) == NamedTuple{(:a,), Tuple{Int}}
+
+  @test prototype(("a",), (Int,)) == NamedTuple{(:a,), Tuple{Int}}
+  @test prototype(("a", "b"), (Int, String)) == NamedTuple{(:a, :b), Tuple{Int, String}}
+  @test prototype(["a", "b"], (Int, String)) == NamedTuple{(:a, :b), Tuple{Int, String}}
+
+  @test prototype(("a",), [Int,]) == NamedTuple{(:a,), Tuple{Int}}
+  @test prototype(("a", "b"), [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
+  @test prototype(["a", "b"], [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
+end
+          
 
   
