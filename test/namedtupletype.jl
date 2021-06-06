57bab2fc39389c23c605499f3be270f764509ddd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-06 10:11:51 -0500

    Update namedtupletype.jl

--- test/namedtupletype.jl
+++ test/namedtupletype.jl
@@ -31,3 +31,21 @@
 @test ntt(("a", "b"), [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
 @test ntt([:a, :b], [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
 @test ntt(["a", "b"], [Int, String]) == NamedTuple{(:a, :b), Tuple{Int, String}}
+
+#=
+@test nt() == NamedTuple{(), Tuple{}}()
+@test nt(:a) == NamedTuple{(:a,)}
+@test nt("a") == NamedTuple{(:a,)}
+@test nt(:a, :b) == NamedTuple{(:a, :b)}
+@test nt("a", "b") == NamedTuple{(:a, :b)}
+
+@test nt((:a,)) == NamedTuple{(:a,)}
+@test nt(("a",)) == NamedTuple{(:a,), Tuple{String}}("a")
+@test nt((:a, :b)) == NamedTuple{(:a, :b)}
+@test nt(("a", "b")) == NamedTuple{(:a, :b), Tuple{String, String}}("a", "b")
+@test nt([:a, :b]) == NamedTuple{(:a, :b)}
+@test nt(["a", "b"]) == NamedTuple{(:a, :b), Tuple{String, String}}("a", "b")
+
+@test nt(:a, x::T) where {T} == NamedTuple{(:a,), Tuple{T}}(z)
+@test nt("a", x::T) where {T} == NamedTuple{(:a,), Tuple{T}}(z)
+=#
