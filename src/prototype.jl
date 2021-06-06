55da9e603dfeb11740694016d9829538a70b4261
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-06 10:37:25 -0500

    Update prototype.jl

--- src/prototype.jl
+++ src/prototype.jl
@@ -17,6 +17,8 @@ speicifying types work with any length-matched tuple of values.
 | NTP  | NamedTuple Prototype | NamedTuple{N}             |
 """ prototype
 
+# prototype as a constructor from wholes
+
 prototype(x::NamedTuple{N,T}; types::Bool=false) where {N,T} =
     ifelse(types, NamedTuple{N,T}, NamedTuple{N})
 
@@ -30,3 +32,36 @@ prototype(x::NamedTuple{N,T}, types::NTuple{N,Type}) where {N,T} =
 
 prototype(x::Type{NamedTuple{N,T}}, types::NTuple{N,Type}) where {N,T} =
     NamedTuple{N, Tuple{types...}}
+
+# `prototype` as a constructor from parts
+
+prototype(x::Symbol) = NamedTuple{(x,)}
+prototype(x::NTuple{N,Symbol}) where {N} = NamedTuple{x}
+prototype(x::Vararg{Symbol}) = NamedTuple{x}
+prototype(x::AbstractVector{Symbol}) = prototype(Tuple(x))
+
+prototype(x::String) = prototype(Symbol(x))
+prototype(x::NTuple{N,String}) where {N} = prototype(Symbol.(x))
+prototype(x::Vararg{String}) = prototype(Symbol.(x))
+prototype(x::AbstractVector{String}) = prototype(Tuple(x))
+
+prototype(x::Symbol, y::Type) = NamedTuple{(x,), Tuple{y}}
+prototype(x::Pair{Symbol,Type}) = prototype(first(x), last(x))
+prototype(x::Tuple{Symbol,Type}) = prototype(first(x), last(x))
+
+prototype(x::NTuple{N,Symbol}, y::NTuple{N,Type}) = NamedTuple{x, Tuple{y...})
+    
+prototype(x::NTuple{N,Symbol}) where {N} = NamedTuple{x}
+prototype(x::Vararg{Symbol}) = NamedTuple{x}
+prototype(x::AbstractVector{Symbol}) = prototype(Tuple(x))
+
+prototype(x::String) = prototype(Symbol(x))
+prototype(x::NTuple{N,String}) where {N} = prototype(Symbol.(x))
+prototype(x::Vararg{String}) = prototype(Symbol.(x))
+prototype(x::AbstractVector{String}) = prototype(Tuple(x))
+
+prototype(x::String, y::Type) = prototype(Symbol(x), y)
+prototype(x::Pair{String,Type}) = prototype(first(x), last(x))
+prototype(x::Tuple{String,Type}) = prototype(first(x), last(x))
+
+prototype(x::NTuple{N,String}, y::NTuple{N,Type}) = prototype{Symbol.(x), y)
