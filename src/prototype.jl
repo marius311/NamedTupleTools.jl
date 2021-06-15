8ac2818014a04a573d70439326651f2be93798cb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-15 10:21:01 -0400

    Update prototype.jl

--- src/prototype.jl
+++ src/prototype.jl
@@ -33,16 +33,16 @@ prototype(@nospecialize names::Vararg{Symbol,N}) where {N} = prototype(names)
 prototype(@nospecialize names::AbstractVector{Symbol}) = prototype(Tuple(names))
 
 # prototype as constructor from 1 name and 1 type
-prototype(name::Symbol, type::Type) = NamedTuple{(name,), Tuple{type}}
-prototype(name::Symbol, type::Tuple{<:Type}) = NamedTuple{(name,), Tuple{first(type)}}
-prototype(name::Tuple{Symbol}, type::Type) = NamedTuple{name, Tuple{type}}
-prototype(name::Tuple{Symbol}, type::Tuple{<:Type}) = NamedTuple{name, Tuple{first(type)}}
+prototype(@nospecialize name::Symbol, @nospecialize type::Type) = NamedTuple{(name,), Tuple{type}}
+prototype(@nospecialize name::Symbol, @nospecialize type::Tuple{<:Type}) = NamedTuple{(name,), Tuple{first(type)}}
+prototype(@nospecialize name::Tuple{Symbol}, @nospecialize type::Type) = NamedTuple{name, Tuple{type}}
+prototype(@nospecialize name::Tuple{Symbol}, @nospecialize type::Tuple{<:Type}) = NamedTuple{name, Tuple{first(type)}}
     
 # prototype as constructor from 2+ names and types
-prototype(names::NTuple{N,Symbol}, types::NTuple{N,Type}) where {N} = NamedTuple{names, Tuple{types...}}
-prototype(names::AbstractVector{Symbol}, types::AbstractVector{<:Type}) = prototype(Tuple(names), Tuple(types))
-prototype(names::NTuple{N,Symbol}, types::AbstractVector{<:Type}) where {N} = prototype(names, Tuple(types))
-prototype(names::AbstractVector{Symbol}, types::NTuple{N,Type}) where {N} = prototype(Tuple(names), types)
+prototype(@nospecialize names::NTuple{N,Symbol}, @nospecialize types::NTuple{N,Type}) where {N} = NamedTuple{names, Tuple{types...}}
+prototype(@nospecialize names::AbstractVector{Symbol}, @nospecialize types::AbstractVector{<:Type}) = prototype(Tuple(names), Tuple(types))
+prototype(@nospecialize names::NTuple{N,Symbol}, @nospecialize types::AbstractVector{<:Type}) where {N} = prototype(names, Tuple(types))
+prototype(@nospecialize names::AbstractVector{Symbol}, @nospecialize types::NTuple{N,Type}) where {N} = prototype(Tuple(names), types)
 
 #=
 # prototype as a constructor from name and type
