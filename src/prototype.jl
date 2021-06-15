7a0d245d6ad3247721d3f0b5db9c38240f835d91
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-15 04:03:53 -0400

    Update prototype.jl

--- src/prototype.jl
+++ src/prototype.jl
@@ -36,6 +36,8 @@ prototype(names::Vararg{Symbol,N}) where {N} =
 
 # prototype as a constructor from names and types
 prototype(names::Symbol, types::DataType) = NamedTuple{(names,), Tuple{types}}
+prototype(names::Symbol, types::Tuple{DataType}) = NamedTuple{(names,), Tuple{types}}
+
 prototype(names::NTuple{N,Symbol}, types::Type{<:Tuple}) where {N} =
     NamedTuple{names, types}
 prototype(names::NTuple{N,Symbol}, types::NTuple{N,DataType}) where {N} =
