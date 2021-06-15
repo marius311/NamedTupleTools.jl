945a89da8e84e69f18bffbf19dd838e4d3f48f97
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-15 03:56:13 -0400

    Update prototype.jl

--- test/prototype.jl
+++ test/prototype.jl
@@ -22,7 +22,7 @@ speicifying types work with any length-matched tuple of values.
 
 @testset "prototype(_)" begin
   @test prototype(Test_NTP) == Test_NTP
-  @test prototype(Test_NTT) == Test_NTP
+  @test prototype(Test_NTT) == Test_NTT
   @test prototype(Test_NT)  == Test_NTP
   @test prototype(test_nt)  == Test_NTP
 end
