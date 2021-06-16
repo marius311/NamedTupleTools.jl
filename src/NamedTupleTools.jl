b0fb7ae4b7ac74a4622308bd84a74ecded98dd00
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-15 20:44:02 -0400

    Update NamedTupleTools.jl

--- src/NamedTupleTools.jl
+++ src/NamedTupleTools.jl
@@ -2,19 +2,19 @@ module NamedTupleTools
 
 export NTT,                            # dispatch on a NamedTuple Type
     prototype,                         # forming NamedTuple Types
-    namedtuple,                        # harmonius multifaceted tooling
+    namedtuple,                        # harmonious multifaceted tooling
+    # support a canonical internal ordering for NamedTuples and their types
+    #    lexicographic over field names `sort(nt::NamedTuple)`
+    #    lexicographic over field names `sort(ntt::Type{NamedTuple})`
+    #    bijection(nt1, nt2) <--> sort(nt1) == sort
+    # data structure interconversions, args follow `convert` (avoids piracy)
+    bijection, ↔,
     # largest fieldcounts supporting each of 3 levels of additional performance
     FastestFieldsMax, FasterFieldsMax,  FastFieldsMax,
     # familiar field operations, expanded applicability      (avoids piracy)
     field_count, field_names,          # all  NamedTupleTypes, all NamedTuples
     field_types, field_tupletypes,     # some NamedTupleTypes, all NamedTuples
     field_values,                      # none NamedTupleTypes, all NamedTuples
-    # support a canonical internal ordering for NamedTuples and their types
-    #    lexicographic over field names `sort(nt::NamedTuple)`
-    #    lexicographic over field names `sort(ntt::Type{NamedTuple})`
-    #    issame(nt1, nt2) <--> sort(nt1) == sort
-    issame,
-    # data structure interconversions, args follow `convert` (avoids piracy)
     restructure,                       # restructure(::Type{Target}, x::Source)
     # incorporate fields not already present by name
     prepend, postpend,                 # add as first [last] field
