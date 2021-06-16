0d1047ff4d39dda1ff051802970645061376ba18
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-16 06:27:50 -0400

    Update support.jl

--- test/support.jl
+++ test/support.jl
@@ -1,19 +1,19 @@
-@testset "bijection" begin
-  @test bijection( (a=1,), (a=1,) )
-  @test bijection( (a=1, b=2), (a=1, b=2) )
-  @test bijection( (a=1, b=2), (b=2, a=1) )
+@testset "isbijection" begin
+  @test isbijection( (a=1,), (a=1,) )
+  @test isbijection( (a=1, b=2), (a=1, b=2) )
+  @test isbijection( (a=1, b=2), (b=2, a=1) )
   
-  @test bijection( (a=1, b=0x02, c='3', d=4.0), (b=0x02, d=4.0, c='3', a=1) )
-  @test !bijection( (a=1, b=0x02, c='3', d=4.0), (b=0x02, d=4.0f0, c='3', a=1) )
+  @test isbijection( (a=1, b=0x02, c='3', d=4.0), (b=0x02, d=4.0, c='3', a=1) )
+  @test !isbijection( (a=1, b=0x02, c='3', d=4.0), (b=0x02, d=4.0f0, c='3', a=1) )
 end
 
 @testset "↔" begin
-  @test (a=1,)  ↔ (a=1,)
-  @test (a=1, b=2)  ↔ (a=1, b=2)
-  @test (a=1, b=2)  ↔ (b=2, a=1)
+  @test (a=1,) ↔ (a=1,)
+  @test (a=1, b=2) ↔ (a=1, b=2)
+  @test (a=1, b=2) ↔ (b=2, a=1)
   
-  @test (a=1, b=0x02, c='3', d=4.0)  ↔ (b=0x02, d=4.0, c='3', a=1)
-  @test !( (a=1, b=0x02, c='3', d=4.0)  ↔ (b=0x02, d=4.0f0, c='3', a=1) )
+  @test (a=1, b=0x02, c='3', d=4.0) ↔ (b=0x02, d=4.0, c='3', a=1)
+  @test !( (a=1, b=0x02, c='3', d=4.0) ↔ (b=0x02, d=4.0f0, c='3', a=1) )
 end
 
 @testset "canonical(NT)" begin
