c165fe153037e30c8ed4dc41002a125589d32d3e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2021-06-16 06:26:22 -0400

    Update support.jl

--- test/support.jl
+++ test/support.jl
@@ -16,4 +16,16 @@ end
   @test !( (a=1, b=0x02, c='3', d=4.0)  ↔ (b=0x02, d=4.0f0, c='3', a=1) )
 end
 
-  
+@testset "canonical(NT)" begin
+  @test canonical( (a=1,) ) == (a = 1, )
+  @test canonical( (a=1, b=2) ) == (a = 1, b = 2)
+  @test canonical( (b=2, a=1) ) == (a = 1, b = 2)
+end
+
+@testset "canonical(NTT)" begin
+  @test canonical( NamedTuple{(:a,)} ) == NamedTuple{(:a,)}
+  @test canonical( NamedTuple{(:a, :b)} ) == NamedTuple{(:a, :b)}
+  @test canonical( NamedTuple{(:b, :a)} ) == NamedTuple{(:a, :b)}
+  @test canonical( NamedTuple{(:a, :b), Tuple{Char, String}} ) == NamedTuple{(:a, :b), Tuple{Char, String}}
+  @test canonical( NamedTuple{(:b, :a), Tuple{String, Char}} ) == NamedTuple{(:a, :b), Tuple{Char, String}}
+end  
